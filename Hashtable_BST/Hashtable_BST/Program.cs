154fa3a55d83457338eb02fa7da43540deb62676
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-20 16:32:49 +0530

    [Sairam]Added:ability to add the elements in the hash table to find them

---
+++ Hashtable_BST/Hashtable_BST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hashtable_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to hashtable");
+            MyMapNode<string, string> get = new MyMapNode<string, string>(5);
+            get.Add("0", "To");
+            get.Add("1", "Be");
+            get.Add("2", "Or");
+            get.Add("3", "Not");
+            get.Add("4", "To");
+            get.Add("5", "Be");
+            Console.WriteLine(get.GetV("5"));
+            Console.WriteLine(get.GetV("3"));
+        }
+    }
+}
